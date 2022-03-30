a3a256e43c2693b4a231da4c1e0eadc43aa98f18
Amrit Clair <amrit_clair@hotmail.com>
2022-03-30 19:06:05 -0400

    Sketch board added.

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,30 @@
 const container = document.querySelector('.container');
 
-document.documentElement.style.setProperty('--size',16);
+// document.documentElement.style.setProperty('--size',16);
 
 makeGrid(16); 
 
 function makeGrid(size){
     for (let i=0; i<(size*size); i++){
         const div = document.createElement(`div`);
+        div.classList.add("myDiv");
         container.appendChild(div);
     }
 }
+
+const divs = document.querySelectorAll('.myDiv');
+divs.forEach(addingEventListener);
+// divs.forEach(setTimeout(removeEventListener,1));
+
+function addingEventListener(div){
+    div.addEventListener('mouseover', function(){
+        div.classList.add("hover");
+    })
+
+}
+
+// function removeEventListener(div){
+//     div.addEventListener("mouseout", function (){
+//         div.classList.remove("hover");
+//     })
+// }
