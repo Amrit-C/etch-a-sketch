88bf18c617a876cededac1bb114386163bd60f67
Amrit Clair <amrit_clair@hotmail.com>
2022-03-31 17:38:14 -0400

    Reset Grid () added.

--- js/javascript.js
+++ js/javascript.js
@@ -4,27 +4,44 @@ const container = document.querySelector('.container');
 
 makeGrid(16); 
 
+
 function makeGrid(size){
     for (let i=0; i<(size*size); i++){
         const div = document.createElement(`div`);
         div.classList.add("myDiv");
         container.appendChild(div);
     }
+    setListeners();
 }
 
-const divs = document.querySelectorAll('.myDiv');
-divs.forEach(addingEventListener);
-// divs.forEach(setTimeout(removeEventListener,1));
+function setListeners (){
+    const divs = document.querySelectorAll('.myDiv');
+    divs.forEach(addingEventListener);
+    
+    const btns = document.querySelectorAll(".button");
+    btns.forEach(buttonEventListener);
+}
+
 
 function addingEventListener(div){
     div.addEventListener('mouseover', function(){
         div.classList.add("hover");
     })
-
 }
 
-// function removeEventListener(div){
-//     div.addEventListener("mouseout", function (){
-//         div.classList.remove("hover");
-//     })
-// }
+
+function buttonEventListener(btn){
+    btn.addEventListener('click', resetGrid)
+}
+
+
+
+function resetGrid(){
+    container.innerHTML = '';
+    let size = prompt("Please enter size of grid.");
+
+    while((size>100)||(size == 0)){
+        size = prompt("You entered an invalid size. Please enter again");
+    }
+    makeGrid(size);
+}
